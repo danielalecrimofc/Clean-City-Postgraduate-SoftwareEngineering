57dfa807f6fcf7d9e4b4ff87d2dc795eafa7c0fe
Andre0Luis <aluis283@gmail.com>
2024-09-10 21:02:39 -0300

    feate: Base Project

---
+++ src/main/java/br/com/clean_city/CleanCityApplication.java
@@ -0,0 +1,13 @@
+package br.com.clean_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCityApplication.class, args);
+	}
+
+}
